7e8ce3023000109a7ca9e58a0533c9f86f0e2030
Ron Juarez <ronstewartjuarez@gmail.com>
2020-08-20 16:32:58 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        Basic Master from App
+      </div>
+    )
+  }
+}
+
+export default App;
