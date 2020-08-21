6b809d0e5445e6a2f3b6e736fdde7851f8a0ca3c
Ron Juarez <ronstewartjuarez@gmail.com>
2020-08-21 15:37:49 -0400

    added search with console output

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,18 @@ import { FormGroup, FormControl, InputGroup } from 'react-bootstrap'
 import { FaSearch } from "react-icons/fa"
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
+
+  search() {
+    console.log('Current Search', this.state);
+    this.setState({query: ''})
+  }
+
   render() {
     return(
       <div className="App">
@@ -15,9 +27,15 @@ class App extends React.Component {
             <FormControl
               type="text"
               placeholder="Search for an Artist"
+              value={this.state.query}
+              onChange={e =>{this.setState({ query: e.target.value})}}
+              onKeyPress={e => e.key === 'Enter' ? this.search() : e.key}
             />
-            <InputGroup.Append>
+            <InputGroup.Append
+              onClick={() => this.search()}>
+            <div className="App-srchbtn">
               <FaSearch/>
+            </div>            
             </InputGroup.Append>
           </InputGroup>
          </FormGroup>  
