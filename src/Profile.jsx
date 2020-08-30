493f8df438e284b92f93838ae4c98333ec9b7f92
Ron Juarez <ronstewartjuarez@gmail.com>
2020-08-30 13:41:20 -0400

    added temp styling

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -7,18 +7,36 @@ class Profile extends Component {
       name: '',
       followers: {
         total: ''
-      }
-    };
+      },
+      images: [{
+        url: ''
+      }],
+      genres : []
+    }
 
-    if (this.props.artist !== null) { 
-      artist = this.props.artist
-    };
+    artist = this.props.artist !== null ? artist = this.props.artist : artist
 
     return (
-      <div>
-        <div>{artist.name}</div>
-        <div>{artist.followers.total}</div>
+      <div className="profile">
+        <img
+          alt="Profile"
+          className="profile-img"
+          src={artist.images[0].url}
+         /> 
+        <div className="profile-info">
+          <div className="profile-name">{artist.name} </div>
+          <div className="profile-followers">{artist.followers.total}</div>
+          <div className="profile-grenres">
+          { 
+            artist.genres.map((genre, k) =>{
+              return(
+                <span key={k}>{genre}</span>
+              )
+            })
+          }
+        </div>
       </div>
+    </div>
     )
   }
 }
